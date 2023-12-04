ac8ee26aa8f52585b5f373b349d6d2ab4a546c93
jpnunis <141031499+jpnunis@users.noreply.github.com>
2023-12-03 23:16:55 -0300

    Create ex 2 selecao.go

---
+++ ex 2 selecao.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var num1, num2, num3 int
+
+	fmt.Print("Digite o primeiro número inteiro: ")
+	fmt.Scan(&num1)
+
+	fmt.Print("Digite o segundo número inteiro: ")
+	fmt.Scan(&num2)
+
+	fmt.Print("Digite o terceiro número inteiro: ")
+	fmt.Scan(&n
