fa6b43c61d44d573ead51f979929d7d2efcfc007
jpnunis <141031499+jpnunis@users.noreply.github.com>
2023-12-03 23:28:35 -0300

    Create ex 08 rep.go

---
+++ ex 08 rep.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var numero int
+
+	// Solicitar entrada do usuário
+	fmt.Print("Digite um número inteiro positivo: ")
+	fmt.Scan(&numero)
+
+	// Verificar se o número é positivo
+	if numero <= 0 {
+		fm
