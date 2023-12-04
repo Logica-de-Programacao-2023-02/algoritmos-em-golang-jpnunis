5b5d2234f4aaab4ec69a157201678ace932b874e
jpnunis <141031499+jpnunis@users.noreply.github.com>
2023-12-03 23:06:49 -0300

    Update ex 04.go

--- ex 04.go
+++ ex 04.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var num1, num2, num3 float64
+	fmt.Print("Digite o primeiro número: ")
+	fmt.Scan(&num1)
+	fmt.Print("Digite o segundo número: ")
+	fmt.Scan(&num2)
+	fmt.Print("Digite o terceiro número: ")
+	fmt.Scan(&num3)
+	mediaPonderada := (num1*2 + num2*3 + num3*5) / 10
+	fmt.Printf("A média ponderada é: %f\n", mediaPonderada)
 }
